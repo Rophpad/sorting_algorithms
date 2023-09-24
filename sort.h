c6d7eece4d4a633c3acf706df2a3e353572d4306
Rophpad <rophenp@gmail.com>
2023-09-24 16:01:07 -0700

    Implement quicksort sorting algorithm

--- sort.h
+++ sort.h
@@ -30,9 +30,12 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2);
 void swap(int *x, int *y);
+void q_sort_recursive(int *array, size_t start, size_t end, size_t size);
+size_t lomutoPartition(int *array, size_t start, size_t end, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
